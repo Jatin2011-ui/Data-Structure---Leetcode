a14d46189e2741dbe112d0064650166d1f49bbc6
Jatin Prabhakar <jatin.prabhakar-coend@bvp.edu.in>
2026-07-07 00:28:34 +0530

    Time: 0 ms (100%), Space: 15.2 MB (30.6%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        vector<vector<int>> result;
+       // vector<vector<int>> result;
         int n = intervals.size();
         auto lambda = [](vector<int>& vec1, vector<int>& vec2){
             if(vec1[0] == vec2[0]){
@@ -10,13 +10,18 @@ public:
             return vec1[0] < vec2[0];
         };
         sort(begin(intervals),end(intervals),lambda);
-        result.push_back(intervals[0]);
+        //result.push_back(intervals[0]);
+        int lastintervalkaEnd = intervals[0][1];
+        int cnt = 1;
         for(int i=0;i<n;i++){
-            if(result.back()[0] <= intervals[i][0] && result.back()[1] >= intervals[i][1]){
+            if( lastintervalkaEnd >= intervals[i][1]){
                 continue;
             } 
-            result.push_back(intervals[i]);
+            //result.push_back(intervals[i]);
+            lastintervalkaEnd = intervals[i][1];
+            cnt++;
         }
-            return result.size();
+          //  return result.size();
+          return cnt;
     }
 };
