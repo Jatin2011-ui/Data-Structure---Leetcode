a26bf94e495d0fd4dd36fa9ace365fffa9d4cb6d
Jatin Prabhakar <jatin.prabhakar-coend@bvp.edu.in>
2026-07-10 01:08:55 +0530

    Time: 527 ms (5.25%), Space: 415.7 MB (5.53%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
+    void dfs(int curr, int compId,unordered_map<int,vector<int>>& adj, vector<int>& component){
+        component[curr] =compId;
+        for(int& ngbr :adj[curr]){
+            if(component[ngbr] == -1){
+                dfs(ngbr,compId,adj,component);
+            }
+        }
+    }
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        unordered_map<int,vector<int>> adj;
+        for(int i=0; i+1<n;i++){
+            if(nums[i+1] - nums[i] <= maxDiff){
+                adj[i].push_back(i+1);
+                adj[i+1].push_back(i);
+            }
+        }
         vector<int> component(n,-1);
-        int compId=0;
-        component[0] = compId;
-        for(int i=1;i<n;i++){
-            if(nums[i] - nums[i-1] > maxDiff){
+        int compId = 0;
+        for(int i=0;i<n;i++){
+            if(component[i] == -1){
+                dfs(i,compId,adj,component);
                 compId++;
             }
-            component[i] = compId;
         }
         vector<bool> result;
         for(auto& query : queries){
