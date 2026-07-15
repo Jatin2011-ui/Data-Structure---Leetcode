eb9f342ded6c3ffb817782df65978ea326c12ca7
Jatin Prabhakar <jatin.prabhakar-coend@bvp.edu.in>
2026-07-16 00:54:54 +0530

    Time: 1149 ms (44.35%), Space: 292.2 MB (36.52%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,24 +1,34 @@
 class Solution {
     int mod = 1e9+7;
-    int t[201][201][201];
 public:
-    int solve(vector<int>& nums, int i, int first, int second){
-        if(i==nums.size()){
-            bool bothNonempty = (first!=0 && second!=0);
-            bool gcdMatch = (first == second);
-            return (bothNonempty && gcdMatch) ? 1 : 0;
-        }
-        if(t[i][first][second] != -1)
-            return t[i][first][second]; 
-
-        int skip = solve(nums,i+1,first,second);
-        int take1 = solve(nums, i+1, __gcd(first,nums[i]),second);
-        int take2 = solve(nums, i+1, first, __gcd(second,nums[i]));
-
-        return t[i][first][second] = (0LL + skip + take1 + take2) % mod;
-    }
     int subsequencePairCount(vector<int>& nums) {
-        memset(t,-1,sizeof(t));
-     return solve(nums,0,0,0);    
+        int n=nums.size();
+        int maxEl = -1;
+        for(int x:nums)
+            maxEl = max(maxEl,x);
+        vector<vector<int>> prev(maxEl+1,vector<int>(maxEl+1,0));
+        for(int first=maxEl;first >= 0;first--){
+            for(int second = maxEl; second >= 0; second--){
+                bool bothNonempty = (first != 0 && second != 0);
+                bool gcdMatch = (first == second);
+                prev[first][second] = (bothNonempty && gcdMatch) ? 1 : 0;
+            }
+        }   
+        for(int i = n-1; i >= 0; i--){
+            vector<vector<int>> curr(maxEl+1,vector<int>(maxEl+1,0));
+            for(int first = maxEl;first >= 0 ;first--){
+                for(int second = maxEl; second >= 0; second--){
+                    int skip = prev[first][second];
+
+                    int take1 = prev[__gcd(first,nums[i])][second];
+
+                    int take2 = prev[first][__gcd(second,nums[i])];
+
+                    curr[first][second] = (0LL + skip + take1 + take2) % mod;
+                }
+            }
+            prev = move(curr);
+        }
+        return prev[0][0];
     }
 };
