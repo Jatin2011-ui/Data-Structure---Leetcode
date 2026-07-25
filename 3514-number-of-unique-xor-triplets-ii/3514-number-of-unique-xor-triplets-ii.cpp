c8c4c8349cc321b8711cfd2d52a586acd8ac6abc
Jatin Prabhakar <jatin.prabhakar-coend@bvp.edu.in>
2026-07-25 23:28:20 +0530

    Time: 444 ms (77.97%), Space: 48.6 MB (67.24%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -3,18 +3,33 @@ public:
     int uniqueXorTriplets(vector<int>& nums) {
         int n = nums.size();
 
-        unordered_set<int> s1;
+        int maxEl = *max_element(begin(nums),end(nums));
+
+        int T = 1;
+        while(T <= maxEl){
+            T = T*2;
+        }
+        vector<int> s1(T,false); //XOR pair values set to true 
+        vector<int> s2(T,false); // XOR triplets values set to true 
+
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                s1.insert(nums[i] ^ nums[j]);
+            for(int j =0;j<n;j++){
+                s1[nums[i] ^ nums[j]] = true;
             }
         }
-        unordered_set<int> s2;
-        for(int pairXor : s1){
-            for(int &num : nums){
-            s2.insert(pairXor ^ num);
+        for(int i=0;i<T;i++){
+            if(s1[i] == true){
+                for(int &num : nums){
+                    s2[i ^ num] = true;
+                }
             }
         }
-        return s2.size();
+        int cnt = 0;
+        for(int i=0;i<T;i++){
+            if(s2[i] == true ){
+                cnt++;
+            }
+        }
+        return cnt;
     }
 };
