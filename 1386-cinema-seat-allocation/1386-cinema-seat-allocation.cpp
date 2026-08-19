46f5c277d8899efa438bf0c19c194e0b2ce0bc06
Jatin Prabhakar <jatin.prabhakar-coend@bvp.edu.in>
2026-08-19 23:43:43 +0530

    Time: 19 ms (80.74%), Space: 60.3 MB (62.67%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        unordered_map<int,int> mp;
-        for(auto& reservedSeat : reservedSeats){
+        unordered_map<int,int> mp; //row - > seats in each row booked
+        for(auto& reservedSeat : reservedSeats){//O(10^4)
             int row = reservedSeat[0];
             int seat = reservedSeat[1];
 
-            mp[row] |= (1 << seat);
+            mp[row] |= (1 << seat); // sets bits are booked seats
         }
         int result = ( n - mp.size()) * 2;
-        int maskA = (1 << 2) | ( 1 << 3) | (1 << 4) | (1 << 5);
-        int maskB = (1 << 4) | ( 1 << 5) | (1 << 6) | (1 << 7);
-        int maskC = (1 << 6) | ( 1 << 7) | (1 << 8) | (1 << 9);
+        int maskA = (1 << 2) | ( 1 << 3) | (1 << 4) | (1 << 5);//set bits are the ones which I need empty for Group A
+        int maskB = (1 << 4) | ( 1 << 5) | (1 << 6) | (1 << 7);//set bits are the ones which I need empty for Group B
+        int maskC = (1 << 6) | ( 1 << 7) | (1 << 8) | (1 << 9);//set bits are the ones which I need empty for Group C
 
-        for(auto& [row, bookedSeatMask] : mp){
+        for(auto& [row, bookedSeatMask] : mp){//min(10*n, 10^4)
             bool groupA = (bookedSeatMask & maskA) == 0;
             bool groupB = (bookedSeatMask & maskB) == 0;
             bool groupC = (bookedSeatMask & maskC) == 0;
