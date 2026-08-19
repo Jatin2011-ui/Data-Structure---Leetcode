020a29e65d9d8ff6f8909624a122b19801c5950a
Jatin Prabhakar <jatin.prabhakar-coend@bvp.edu.in>
2026-08-19 23:40:34 +0530

    Time: 3 ms (99.66%), Space: 60.1 MB (73.48%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,35 +1,28 @@
-//Approach-1 (Greedy Pick using map)
-//T.C : O(N), N = reservedSeats.size()
-//S.C : O(N), for storing reswerved seats in map (in form of unordered_set)
-
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        unordered_map<int,unordered_set<int>> mp; //row -> seats in each row booked
-
-        for(auto& reservedSeat: reservedSeats){
+        unordered_map<int,int> mp;
+        for(auto& reservedSeat : reservedSeats){
             int row = reservedSeat[0];
             int seat = reservedSeat[1];
 
-            mp[row].insert(seat);
+            mp[row] |= (1 << seat);
         }
-        int result = (n - mp.size()) * 2;
+        int result = ( n - mp.size()) * 2;
+        int maskA = (1 << 2) | ( 1 << 3) | (1 << 4) | (1 << 5);
+        int maskB = (1 << 4) | ( 1 << 5) | (1 << 6) | (1 << 7);
+        int maskC = (1 << 6) | ( 1 << 7) | (1 << 8) | (1 << 9);
 
-        for(auto& [row, bookedSeats] : mp){
-        auto isAvaiable = [&](int seat){
-            return bookedSeats.find(seat) == bookedSeats.end();
-        };
+        for(auto& [row, bookedSeatMask] : mp){
+            bool groupA = (bookedSeatMask & maskA) == 0;
+            bool groupB = (bookedSeatMask & maskB) == 0;
+            bool groupC = (bookedSeatMask & maskC) == 0;
 
-        bool groupA = isAvaiable(2) && isAvaiable(3) && isAvaiable(4) && isAvaiable(5);
-        bool groupB = isAvaiable(4) && isAvaiable(5) && isAvaiable(6) && isAvaiable(7);
-        bool groupC = isAvaiable(6) && isAvaiable(7) && isAvaiable(8) && isAvaiable(9);
-
-        if(groupA && groupC)
-            result += 2;
-        else if(groupA | groupB | groupC)
-            result += 1;
+            if(groupA && groupC)
+                result += 2;
+            else if(groupA || groupB || groupC)
+                result += 1;
         }
-
-    return result;
+        return result;
     }
 };
