71f89406b9d3c5767bc5367ef16c3b065f63a502
Jatin Prabhakar <jatin.prabhakar-coend@bvp.edu.in>
2026-08-26 23:56:47 +0530

    Time: 7 ms (12.09%), Space: 11.3 MB (6.55%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,29 +1,32 @@
-
-//sliding windows technique
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
         int n = s.length();
-        int i=0;
-        int ones = 0;//number of '1's in window [i,j]
-        string result = "";
-        
-        for(int j=0;j<n;j++){
-            if(s[j] == '1')
-                ones++;
-            //remove extras 1's , then trim leading 0's
-            while(ones>k || s[i] =='0'){
-                if(s[i] == '1')
-                    ones--;
-                i++;
-            }
-            if(ones == k){
-                string temp = s.substr(i,j-i+1);
-                if(result.empty() || j-i+1 < result.length() || (j-i+1 == result.length() && temp < result)){
-                    result = temp;
+
+        for(int len = k;len <=n ; len++){
+            string result = "";
+
+            for(int start=0;start+len <= n; start++){//trying all possible substr of len 
+                string temp = s.substr(start,len); //[start.....start+len]
+
+                int ones = 0;
+                for(char ch: temp){
+                    ones += (ch == '1') ? 1 : 0;
                 }
-            }
+
+                //kkeep it if it's beautifuland smaller than current best.
+                if(ones == k){
+                    if(result.empty() || temp < result)
+                        result = temp;
+                }
+                }
+
+                //if we find the result of k size, then it's smallest , no need to move to k++
+                if(!result.empty())
+                    return result;
+                }
+                
+            return "";
         }
-        return result;
-    }
+    
 };
